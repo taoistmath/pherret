0d860e592747db6c3470742e0849372657e02d70
taoistmath <greg.fogelberg@gmail.com>
2013-09-17 13:11:06 -0700

    bootstrap in and buttons aligned

--- pherretResults.php
+++ pherretResults.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php
 /**
  * Created by JetBrains PhpStorm.
@@ -7,7 +8,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-//session_start();
+session_start();
 
 //Timestamp for creating HTML file for test results
 $behatLoc = 'tools/regression/'; //Set to relative path to location of behat.yml file
@@ -48,6 +49,90 @@ if($username == "")
     removeFilterFromFeature($features);
 
 }
+
+?>
+
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <title>PHERRET</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <style>
+        body {
+            padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+        }
+    </style>
+    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="../assets/ico/favicon.png">
+</head>
+
+<body>
+
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="brand" href="#">PHp Engineering Repository REgression Tool (PHERRET)</a>
+
+            <div class="nav-collapse collapse">
+                <ul class="nav">
+                    <!--                    <li class="active"><a href="#">Home</a></li>-->
+                    <!--                    <li><a href="#about">About</a></li>-->
+                    <!--                    <li><a href="#contact">Contact</a></li>-->
+                    <!--                    <li><a href="/logout.php">Sign Out</a></li>-->
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+</div>
+
+<div class="container">
+
+    <h1>PHERRET</h1>
+<div class="btn-group">
+<p>
+    <form id="featureFilter" name="featureFilter" method="GET" action="pherret.php">
+        <div class="span2">
+            <label><br></label>
+            <button class="btn btn-primary" type="submit">Return to List</button>
+            <label><br></label>
+        </div>
+    </form>
+
+    <form id="deleteOldFile" name="deleteOldFile" method="GET" action="deleteOldFile.php">
+        <div class="span2">
+            <label><br></label>
+            <button class="btn btn-danger" type="submit">Delete Old Files</button>
+            <label><br></label>
+        </div>
+    </form>
+    </p>
+    </div>
+</div>
+
+
+<?php
 
 function commitExecution()
 {
@@ -129,20 +214,3 @@ function removeFilterFromFeature($features)
 
 ?>
 
-<form id="featureFilter" name="featureFilter" method="GET" action="pherret.php">
-
-    <div class="span2">
-        <label><br></label>
-        <button class="btn btn-primary" type="submit">Return to List</button>
-        <label><br></label>
-    </div>
-
-</form>
-
-<form id="deleteOldFile" name="deleteOldFile" method="GET" action="deleteOldFile.php">
-    <div class="span2">
-        <label><br></label>
-        <button class="btn btn-primary" type="submit">Delete Old Files</button>
-        <label><br></label>
-    </div>
-</form>
