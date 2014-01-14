f762f0bbd053aa8ae173e55e48410a2dc72d58ce
taoistmath <greg.fogelberg@gmail.com>
2014-01-14 14:28:09 -0800

    marking out db configs for local file that will not be checked in

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -2,13 +2,18 @@
 /**
  * These are the database login details
  */  
-define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "*****");    // The database username. 
-define("PASSWORD", "*****");    // The database password. 
-define("DATABASE", "*****");    // The database name.
+
+// Please set to your own configuration
+// define("HOST", "localhost");     // The host you want to connect to.
+// define("USER", "*****");    // The database username. 
+// define("PASSWORD", "*****");    // The database password. 
+// define("DATABASE", "*****");    // The database name.
  
-define("CAN_REGISTER", "any");
-define("DEFAULT_ROLE", "member");
+// define("CAN_REGISTER", "any");
+// define("DEFAULT_ROLE", "member");
  
-define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+// define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+
+include_once('local-psl-config.php');
+
 ?>
